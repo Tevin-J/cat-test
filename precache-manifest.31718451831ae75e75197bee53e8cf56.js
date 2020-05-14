b4843948bcad570b935434531c17204237e695f3
Anton <anton.revta@gmail.com>
2020-05-14 23:56:00 +0300

    Updates

---
+++ precache-manifest.31718451831ae75e75197bee53e8cf56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104d6e7d96c34e935bb3517252c83a7d",
+    "url": "/cat-test/index.html"
+  },
+  {
+    "revision": "d10130208e63780db78a",
+    "url": "/cat-test/static/css/main.eb5f79b6.chunk.css"
+  },
+  {
+    "revision": "3f91c45ff933ea74b942",
+    "url": "/cat-test/static/js/2.556ee4ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cat-test/static/js/2.556ee4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10130208e63780db78a",
+    "url": "/cat-test/static/js/main.6b8fcf81.chunk.js"
+  },
+  {
+    "revision": "36a40c14a693a35e9fb6",
+    "url": "/cat-test/static/js/runtime-main.7a831afa.js"
+  }
+]);
